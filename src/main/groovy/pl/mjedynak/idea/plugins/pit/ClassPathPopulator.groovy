18513f0f17fb354e21b6a173f170da80d6349d2b
Michal Jedynak <m.jedynak@gmail.com>
2014-01-26 19:49:34 +0100

    added test

--- src/main/groovy/pl/mjedynak/idea/plugins/pit/ClassPathPopulator.groovy
+++ src/main/groovy/pl/mjedynak/idea/plugins/pit/ClassPathPopulator.groovy
@@ -8,10 +8,12 @@ class ClassPathPopulator {
     static final String PITEST_JAR = "pitest-0.32.jar"
     static final String PITEST_COMMAND_LINE_JAR = "pitest-command-line-0.32.jar"
     static final String SEPARATOR = System.getProperty("file.separator")
+    static final String PLUGIN_NAME = "pit-idea-plugin"
+    static final String LIB_DIR = "lib"
 
     void populateClassPathWithPitJar(PathsList classPath) {
         String pluginsPath = PathManager.getPluginsPath()
-        String path = pluginsPath + SEPARATOR + "pit-idea-plugin" + SEPARATOR + "lib" + SEPARATOR
+        String path = pluginsPath + SEPARATOR + PLUGIN_NAME + SEPARATOR + LIB_DIR + SEPARATOR
         classPath.add(path + PITEST_JAR)
         classPath.add(path + PITEST_COMMAND_LINE_JAR)
     }
---
+++ src/test/groovy/pl/mjedynak/idea/plugins/pit/ClassPathPopulatorTest.groovy
@@ -0,0 +1,25 @@
+package pl.mjedynak.idea.plugins.pit
+
+import com.intellij.openapi.application.PathManager
+import com.intellij.util.PathsList
+import spock.lang.Specification
+
+import static pl.mjedynak.idea.plugins.pit.ClassPathPopulator.*
+
+class ClassPathPopulatorTest extends Specification {
+
+    ClassPathPopulator classPathPopulator = new ClassPathPopulator()
+    PathsList classPath = Mock()
+    private final static String PLUGINS_PATH = "path"
+
+    def "should populate classpath"() {
+        PathManager.metaClass.static.getPluginsPath = { PLUGINS_PATH }
+
+        when:
+        classPathPopulator.populateClassPathWithPitJar(classPath)
+
+        then:
+        1 * classPath.add(PLUGINS_PATH + SEPARATOR + PLUGIN_NAME + SEPARATOR + LIB_DIR + SEPARATOR + PITEST_JAR)
+        1 * classPath.add(PLUGINS_PATH + SEPARATOR + PLUGIN_NAME + SEPARATOR + LIB_DIR + SEPARATOR + PITEST_COMMAND_LINE_JAR)
+    }
+}
