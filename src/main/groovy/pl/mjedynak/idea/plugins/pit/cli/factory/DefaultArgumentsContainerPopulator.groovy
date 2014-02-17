73a68b855cb38b03e4e85b92e915145d3a44035f
Michal Jedynak <m.jedynak@gmail.com>
2014-02-17 22:02:35 +0100

    removed semicolons

--- src/main/groovy/pl/mjedynak/idea/plugins/pit/cli/factory/DefaultArgumentsContainerPopulator.groovy
+++ src/main/groovy/pl/mjedynak/idea/plugins/pit/cli/factory/DefaultArgumentsContainerPopulator.groovy
@@ -19,92 +19,91 @@ import static pl.mjedynak.idea.plugins.pit.cli.model.PitCommandLineArgument.TARG
 @CompileStatic
 class DefaultArgumentsContainerPopulator {
 
-    static final String DEFAULT_REPORT_DIR = "report";
-    static final String MAVEN_REPORT_DIR = "target/report";
-    static final String GRADLE_REPORT_DIR = "build/reports/pit";
-    static final String ALL_CLASSES_SUFFIX = ".*";
+    static final String DEFAULT_REPORT_DIR = "report"
+    static final String MAVEN_REPORT_DIR = "target/report"
+    static final String GRADLE_REPORT_DIR = "build/reports/pit"
+    static final String ALL_CLASSES_SUFFIX = ".*"
 
-    private ProjectRootManager projectRootManager;
-    private PsiManager psiManager;
-    private MavenProjectDeterminer mavenProjectDeterminer = new MavenProjectDeterminer();
-    private GradleProjectDeterminer gradleProjectDeterminer = new GradleProjectDeterminer();
-    private MavenPomReader mavenPomReader = new MavenPomReader();
+    private ProjectRootManager projectRootManager
+    private PsiManager psiManager
+    private MavenProjectDeterminer mavenProjectDeterminer = new MavenProjectDeterminer()
+    private GradleProjectDeterminer gradleProjectDeterminer = new GradleProjectDeterminer()
+    private MavenPomReader mavenPomReader = new MavenPomReader()
 
     DefaultArgumentsContainerPopulator(ProjectRootManager projectRootManager, PsiManager psiManager) {
-        this.projectRootManager = projectRootManager;
-        this.psiManager = psiManager;
+        this.projectRootManager = projectRootManager
+        this.psiManager = psiManager
     }
 
     void addReportDir(Project project, PitCommandLineArgumentsContainer container) {
-        VirtualFile baseDir = project.getBaseDir();
-        String reportDir;
+        VirtualFile baseDir = project.getBaseDir()
+        String reportDir
         if (baseDir != null) {
-            String suffix = DEFAULT_REPORT_DIR;
+            String suffix = DEFAULT_REPORT_DIR
             if (mavenProjectDeterminer.isMavenProject(project)) {
-                suffix = MAVEN_REPORT_DIR;
+                suffix = MAVEN_REPORT_DIR
             } else if (gradleProjectDeterminer.isGradleProject(project)) {
-                suffix = GRADLE_REPORT_DIR;
+                suffix = GRADLE_REPORT_DIR
             }
-            reportDir = baseDir.getPath() + "/" + suffix;
-            container.put(PitCommandLineArgument.REPORT_DIR, reportDir);
+            reportDir = baseDir.getPath() + "/" + suffix
+            container.put(PitCommandLineArgument.REPORT_DIR, reportDir)
         }
     }
 
     void addSourceDir(PitCommandLineArgumentsContainer container) {
-        VirtualFile[] sourceRoots = projectRootManager.getContentSourceRoots();
+        VirtualFile[] sourceRoots = projectRootManager.getContentSourceRoots()
         VirtualFile javaSrcFolder = sourceRoots.find { VirtualFile sourceRoot ->
             sourceRoot.getPath().contains("java")
         }
         if (javaSrcFolder != null) {
-            container.put(SOURCE_DIRS, javaSrcFolder.getPath());
+            container.put(SOURCE_DIRS, javaSrcFolder.getPath())
         } else if (hasAtLeastOneSourceRoot(sourceRoots)) {
-            String sourceRootPath = sourceRoots[0].getPath();
-            container.put(SOURCE_DIRS, sourceRootPath);
+            String sourceRootPath = sourceRoots[0].getPath()
+            container.put(SOURCE_DIRS, sourceRootPath)
         }
     }
 
     void addTargetClasses(Project project, PitCommandLineArgumentsContainer container) {
         if (mavenProjectDeterminer.isMavenProject(project)) {
-            addTargetClassesForMavenProject(project, container);
+            addTargetClassesForMavenProject(project, container)
         } else {
-            addTargetClassesForNonMavenProject(container);
+            addTargetClassesForNonMavenProject(container)
         }
     }
 
     private void addTargetClassesForMavenProject(Project project, PitCommandLineArgumentsContainer container) {
-        VirtualFile baseDir = project.getBaseDir();
-        VirtualFile pomVirtualFile = baseDir.findChild(MavenProjectDeterminer.POM_FILE);
+        VirtualFile baseDir = project.getBaseDir()
+        VirtualFile pomVirtualFile = baseDir.findChild(MavenProjectDeterminer.POM_FILE)
         try {
-            String groupId = mavenPomReader.getGroupId(pomVirtualFile.getInputStream());
-            container.put(TARGET_CLASSES, groupId + ALL_CLASSES_SUFFIX);
+            String groupId = mavenPomReader.getGroupId(pomVirtualFile.getInputStream())
+            container.put(TARGET_CLASSES, groupId + ALL_CLASSES_SUFFIX)
         } catch (Exception e) {
-            throw new IllegalStateException(e);
+            throw new IllegalStateException(e)
         }
     }
 
     private void addTargetClassesForNonMavenProject(PitCommandLineArgumentsContainer container) {
-        VirtualFile[] sourceRoots = projectRootManager.getContentSourceRoots();
+        VirtualFile[] sourceRoots = projectRootManager.getContentSourceRoots()
         if (hasAtLeastOneSourceRoot(sourceRoots)) {
-            PsiDirectory directory = psiManager.findDirectory(sourceRoots[0]);
-            addTargetClassesIfDirectoryExists(container, directory);
+            PsiDirectory directory = psiManager.findDirectory(sourceRoots[0])
+            addTargetClassesIfDirectoryExists(container, directory)
         }
     }
 
-    private
-    static void addTargetClassesIfDirectoryExists(PitCommandLineArgumentsContainer container, PsiDirectory directory) {
+    private static void addTargetClassesIfDirectoryExists(PitCommandLineArgumentsContainer container, PsiDirectory directory) {
         if (directory != null) {
-            PsiDirectory[] subdirectories = directory.getSubdirectories();
+            PsiDirectory[] subdirectories = directory.getSubdirectories()
             if (hasAtLeastOneSubdirectory(subdirectories)) {
-                container.put(TARGET_CLASSES, subdirectories[0].getName() + ALL_CLASSES_SUFFIX);
+                container.put(TARGET_CLASSES, subdirectories[0].getName() + ALL_CLASSES_SUFFIX)
             }
         }
     }
 
     private static boolean hasAtLeastOneSourceRoot(VirtualFile[] sourceRoots) {
-        return !isEmpty(sourceRoots);
+        return !isEmpty(sourceRoots)
     }
 
     private static boolean hasAtLeastOneSubdirectory(PsiDirectory[] subdirectories) {
-        return !isEmpty(subdirectories);
+        return !isEmpty(subdirectories)
     }
 }
