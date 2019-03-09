5c7b3ea2cb695b1433f35bdc597e02d814c5b2d2
mjedynak <m.jedynak@gmail.com>
2019-03-09 22:26:51 +0100

    fix flicker

--- src/test/groovy/pl/mjedynak/idea/plugins/pit/console/DirectoryReaderTest.groovy
+++ src/test/groovy/pl/mjedynak/idea/plugins/pit/console/DirectoryReaderTest.groovy
@@ -4,18 +4,25 @@ import com.google.common.base.Optional
 import spock.lang.Specification
 
 import static com.google.common.io.Files.createTempDir
+import static java.util.concurrent.TimeUnit.MILLISECONDS
 
 class DirectoryReaderTest extends Specification {
 
     DirectoryReader directoryReader = new DirectoryReader()
 
     def "should read latest directory from given parent directory"() {
-        createTempDir().deleteOnExit()
-        File newerDir = createTempDir()
+        File tempDir = createTempDir()
+        tempDir.deleteOnExit()
+        File olderDir = new File(tempDir, "older")
+        olderDir.mkdir()
+        olderDir.deleteOnExit()
+        MILLISECONDS.sleep(1)
+        File newerDir = new File(tempDir, "newer")
+        newerDir.mkdir()
         newerDir.deleteOnExit()
 
         when:
-        Optional<File> result = directoryReader.getLatestDirectoryFrom(new File(System.getProperty('java.io.tmpdir')))
+        Optional<File> result = directoryReader.getLatestDirectoryFrom(tempDir)
 
         then:
         result.get() == newerDir
