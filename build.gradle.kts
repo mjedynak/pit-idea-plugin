5ca0167e02ced1bb3ab5a906aa95637700108187
mjedynak <m.jedynak@gmail.com>
2026-07-25 22:41:04 +0200

    feat: update PIT to 1.20.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.diffplug.spotless") version "8.8.0"
 }
 
-val pitVersion = "1.20.0"
+val pitVersion = "1.20.7"
 val pitJunit5PluginVersion = "1.2.3"
 
 java {
@@ -37,13 +37,11 @@ dependencies {
     implementation("org.pitest:pitest-entry:$pitVersion") { isTransitive = false }
     implementation("org.pitest:pitest:$pitVersion") { isTransitive = false }
     implementation("org.pitest:pitest-junit5-plugin:$pitJunit5PluginVersion")
-    implementation("org.apache.commons:commons-text:1.10.0")
+    implementation("org.apache.commons:commons-text:1.14.0")
     implementation("org.junit.platform:junit-platform-launcher:1.9.2") { isTransitive = false }
-    implementation("com.google.guava:guava:32.1.3-jre")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
     testImplementation("org.mockito.kotlin:mockito-kotlin:6.3.0")
     testImplementation("org.mockito:mockito-core:5.23.0")
-    testRuntimeOnly("junit:junit:4.13.2")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- src/main/kotlin/pl/mjedynak/idea/plugins/pit/ClassPathPopulator.kt
+++ src/main/kotlin/pl/mjedynak/idea/plugins/pit/ClassPathPopulator.kt
@@ -2,6 +2,7 @@ package pl.mjedynak.idea.plugins.pit
 
 import com.intellij.openapi.application.PathManager
 import com.intellij.util.PathsList
+import java.nio.file.FileSystems
 
 class ClassPathPopulator {
     fun populateClassPathWithPitJar(classPath: PathsList) {
@@ -11,8 +12,8 @@ class ClassPathPopulator {
             addFirst("${path}pitest-$PITEST_VERSION.jar")
             addFirst("${path}pitest-command-line-$PITEST_VERSION.jar")
             addFirst("${path}pitest-entry-$PITEST_VERSION.jar")
-            addFirst("${path}commons-lang3-3.12.0.jar")
-            addFirst("${path}commons-text-1.10.0.jar")
+            addFirst("${path}commons-text-1.14.0.jar")
+            addFirst("${path}commons-lang3-3.18.0.jar") // transitive dependency of commons-text
             addFirst("${path}pitest-junit5-plugin-$PITEST_JUNIT5_PLUGIN_VERSION.jar")
             if (noPlatformLauncherDependency(classPath)) {
                 addFirst("${path}junit-platform-launcher-1.9.2.jar")
@@ -24,9 +25,9 @@ class ClassPathPopulator {
         classPath.pathList.none { it.contains("junit-platform-launcher") }
 
     companion object {
-        const val PITEST_VERSION = "1.20.0"
+        const val PITEST_VERSION = "1.20.7"
         const val PITEST_JUNIT5_PLUGIN_VERSION = "1.2.3"
-        val SEPARATOR: String = System.getProperty("file.separator")
+        val SEPARATOR: String = FileSystems.getDefault().separator
         const val PLUGIN_NAME = "pit-idea-plugin"
         const val LIB_DIR = "lib"
     }
